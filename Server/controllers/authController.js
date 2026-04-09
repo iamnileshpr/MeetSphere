d9b5eeddd32b397fd57684b9a6759f92599a2253
iamnileshpr <nilesh.rajak123456@gmail.com>
2026-04-10 00:30:46 +0530

    10th april

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -1,7 +1,7 @@
    //to login
 
    import User from "../model/User";
-
+   import { generateToken } from "../utils/jwt";
    export const register = async(req, res, next) => {
        try {
            const { name, email, password } = req.body;
@@ -19,7 +19,65 @@
                email,
                password
            })
+
+           //generate token
+
+           const token = genrateToken(user._id);
+           res.status(200).json({
+               success: true,
+               data: {
+                   user: {
+                       id: user._id,
+                       name: user.name,
+                       email: user.email
+                   },
+                   token
+               },
+               message: "user registered successfully"
+           })
        } catch (error) {
+           next(error)
+
+       }
+   }
+
+   export const login = async(req, res, next) => {
+       try {
+           const { email, password } = req.body;
+           const user = await User.findOne({ email }).select('+password')
+           if (user) {
+               return res.status(401).json({
+                   success: false,
+                   error: 'Invalid email and password'
+               })
+           }
+           //check if password is matched or not
+
+           const isPasswordMatch = await User.matchPassword(password)
+
+           if (!isPasswordMatch) {
+               return res.status(401).json({
+                   success: false,
+                   error: "invalid"
+               })
+           }
+           //generate token
+
+           const token = genrateToken(user._id);
+           res.status(200).json({
+               success: true,
+               data: {
+                   user: {
+                       id: user._id,
+                       name: user.name,
+                       email: user.email
+                   },
+                   token
+               },
+               message: "user login successfully"
+           })
+       } catch (error) {
+           next(error)
 
        }
    }
--- Server/utils/jwt.js
+++ Server/utils/jwt.js
@@ -1,1 +1,13 @@
-const generateToken
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN })
+}
+
+export const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, process.env.JWT_SECRET)
+    } catch (error) {
+        throw new Error("Invalid or expired token")
+    }
+}
